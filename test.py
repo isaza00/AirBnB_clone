0e6a1a75bbcd0966108eb05438f66d8527a806fe
Norman <isaza00@hotmail.com>
2020-07-01 10:34:28 -0500

    before file_storage change

--- test.py
+++ test.py
@@ -2,19 +2,3 @@
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
-
-my_model = BaseModel()
-my_model.name = "Holberton"
-my_model.my_number = 89
-print(my_model)
-print("______________")
-user = User()
-print("--")
-print(user)
-print("--")
-print(user.to_dict())
-print()
-place = Place()
-print(place)
-print("--")
-print(place.to_dict())
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,8 +2,9 @@
 """Tests Base Model"""
 
 import unittest
-"""import pep"""
 from models.base_model import BaseModel
+from models import storage
+import os
 
 
 class TestsBase(unittest.TestCase):
@@ -15,8 +16,7 @@ class TestsBase(unittest.TestCase):
         result = pep8style.check_files(['./models/base_model.py'])
         self.assertEqual(result.total_errors, 0)"""
 
-    def test_any(self):
-        """Test"""
+    """def test_any(self):
         model = BaseModel()
         str1 = "[BaseModel] "
         str2 = "(" + model.id + ") "
@@ -25,23 +25,32 @@ class TestsBase(unittest.TestCase):
         self.assertEqual(model.__str__(), string)
 
     def test_print(self):
-        """Print ok"""
         model = BaseModel()
         my_model_json = model.to_dict()
         self.assertEqual(my_model_json["id"], model.id)
 
     def test_print1(self):
-        """Print ok"""
         model = BaseModel()
         my_model_json = model.to_dict()
         self.assertEqual(my_model_json["created_at"], model.created_at.isoformat())
 
     def test_print2(self):
-        """Print ok"""
         model = BaseModel()
         my_model_json = model.to_dict()
         self.assertEqual(my_model_json["updated_at"], model.updated_at.isoformat())
-
+    """
+    
     def test_print3(self):
         """Print ok"""
-        self.assertEqual(1, 1)
+        
+        model = BaseModel()
+        model.save()
+        dic = storage.all()
+        print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        print(dic)
+        new_model = BaseModel(**dic)
+        print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        print(model)
+        print(new_model)
+        self.assertEqual(model.to_dict(), new_model.to_dict())
+        self.assertNotEqual(model, new_model)
